90bee1a9c1f8759f1c7b46f4a9f517feebeeade1
Josh Radochoni <jradocho@purdue.u>
2014-11-20 11:50:31 -0500

    modulus functionality

--- calc.py
+++ calc.py
@@ -34,6 +34,8 @@ def div(a, b):
     return a / b
 
 
+def mod(a, b):
+	return a % b
 # -------------------------------------------------------- #
 
 
@@ -67,6 +69,8 @@ while (True):
             print "Product: ", mult(a, b)
         elif (op == "/"):
             print "Quotient: ", div(a, b)
+		elif (op == "%"):
+			print "Modulus: ", mod(a, b)
         else:
             print "Invalid operation..."
 
